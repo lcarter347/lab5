37ad0b74cfe3cc3390d766047806ed6319b92964
Hannah Zontine <hzontine@Hannahs-MacBook-Pro.local>
2016-03-31 12:49:23 -0400

    Adding code to while loop, does not compile currently

--- main.cpp
+++ main.cpp
@@ -18,24 +18,27 @@ int main(){
 	if(!myInputFile.is_open()){
 		myInputFile.open(inputFileName.c_str());
 	}
-	char* line;
-	string num;
-	string val;
+	string line, charNum, charVal;
+	int space;
+	//make array
 	while(!myInputFile.eof()){
 		getline(cin, line);
-     f (command.length() > 2){
-254         for (int i = 0; i < command.length(); i++){
- 255             if (isspace(command[i])){
-256                 space = i;
-257                 break;
-258             } else {
-    259                 space = -1;
-    260             }
- 261         }
-   262         if (space == -1){
-       263             cout << "Incorrect command format" << endl;
+		if (line.length() > 2){
+			for (int i = 0; i < line.length(); i++){
+				if (isspace(line[i])){
+					space = i;
+					break;
+   				}
+ 			}
+			for(int i = 0; i < space; i++){
+				charVal = charVal + line[i];
+			}
+			for(int i = space; i < line.length; i++){
+		
+			}
+		}		
 
-	
+
 		Datawrapper* a = new Datawrapper();
 		a->setCharac(value);
 		a->setWeight(stod(num));
@@ -45,7 +48,7 @@ int main(){
 
 
 
-
+//dynamic array of node pointers
 
 
 

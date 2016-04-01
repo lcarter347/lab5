c93b6af3e126b409e2da645d5532df1624f6fb47
Justin Rice <jrice5@mail.umw.edu>
2016-03-31 20:09:30 -0400

    Fooling with overloading the <> operators

--- datawrapper.h
+++ datawrapper.h
@@ -1,5 +1,6 @@
 
 #include <string>
+#include <iostream>
 
 #ifndef DATAWRAPPER_H_
 #define DATAWRAPPER_H_
@@ -16,7 +17,33 @@ class Datawrapper{
         string getCharac();
         void setWeight(double);
         void setCharac(string);
+        bool operator< (const Datawrapper dw2);
+        bool operator> (const Datawrapper dw2);
+/*
+        friend bool operator<(const Datawrapper &dw2) const;
+        friend bool operator>(const Datawrapper &dw2) const;*/
 };
+
+
+bool Datawrapper::operator< (const Datawrapper dw2){
+    cout << "Made it into less than." << endl;
+    return weight < dw2.weight;
+}
+
+bool Datawrapper::operator> (const Datawrapper dw2){
+    cout << "Made it into greater than." << endl;
+    return weight > dw2.weight;
+}
+/*
+
+bool Datawrapper::operator<(const Datawrapper &dw2) const{
+    return weight<dw2.weight;
+}
+
+bool Datawrapper::operator>(const Datawrapper &dw2) const{
+    return weight>dw2.weight;
+}
+*/
 
 Datawrapper::Datawrapper(){
         double weight;

4d435ce22d740e21517ee45b5084789befea4195
Hannah Zontine <hzontine@Hannahs-MBP.home>
2016-04-03 23:33:27 -0400

    Reading thru the file, assigning value to nodes, dynamic array: Complete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // Justin Rice, Lisa Carter, Hannah Zontine
 // Lab 5
-// Sources:
+// Sources: Stack Overflow, cplusplus.com
 
 #include <iostream>
 #include <fstream>
@@ -19,41 +19,54 @@ int main(){
 		myInputFile.open(inputFileName.c_str());
 	}
 	string line, charNum, charVal;
-	string space; 
-	TreeNode<Datawrapper*>* ptrArray = new TreeNode<Datawrapper*>()[];
+	string space;
+	int lineNumOfFile;
+	TreeNode<Datawrapper*>* ptrArray = new TreeNode<Datawrapper*>();
 	while(!myInputFile.eof()){
 		getline(cin, line);
 		if (line.length() > 2){
+			int spaces;
+			int count = 0;
 			for (int i = 0; i < line.length(); i++){
 				if (isspace(line[i])){
-					space.append(i);
-					cat(i);
-					break;
+					if(count < 1){
+						spaces = i;
+					}
+					count++;
    				}
  			}
-			if(space.length() == 1){
+			if(count == 1){
 				for(int i = 0; i < space[0]; i++){
 					charVal = charVal + line[i];
 				}
-				for(int i = space[0]; i < line.length(); i++){
+				for(int i = space[0]+1; i < line.length(); i++){
 					charNum = charNum + line[i];
 				}
+			}else{
+				cout << "Incorrrect file format on line "<< lineNumOfFile << ".\n"; 
 			}
 		}
 		Datawrapper* a = new Datawrapper();
 		a->setCharac(charVal);
 		a->setWeight(stod(charNum));
 		TreeNode<Datawrapper*>* n = new TreeNode<Datawrapper*>();
-		n->setData(*a);
-		
-		
+		n->setData(a);
 
 
 
 
 
-		break;	
+		lineNumOfFile ++;
+
+
+
+		break;
 	}
+
+
+
+
+
 
 	myInputFile.close();
 	return 0;

24e41d2665f9dfbdff1bf38f33de746124e54f95
Justin Rice <jrice5@mail.umw.edu>
2016-04-07 11:25:42 -0400

    Adding huffman.h

---
+++ huffman.h
@@ -0,0 +1,31 @@
+
+
+class Huffman{
+    private:
+        TreeNode<Datawrapper*> * root;
+
+    public:
+        Huffman();
+        void makeTree(String);
+        String decode(char * data);
+        char * encode(String);
+};
+
+Huffman::Huffman(){
+
+}
+
+void Huffman::makeTree(String filename){
+
+
+}
+
+String Huffman::decode(char * data){
+
+
+}
+
+char * Huffman::encode(String){
+
+
+}

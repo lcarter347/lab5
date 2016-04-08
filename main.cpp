a1961350aa5a95b573d0f9c68a7a49f57a5a0843
Hannah Zontine <hzontine@Hannahs-MBP.home>
2016-04-08 16:46:00 -0400

    Added interface for user

--- main.cpp
+++ main.cpp
@@ -12,11 +12,29 @@
 using namespace std;
 
 int main(){
-	string inputFileName;
-	cout << "Please enter the name of an input file: " << endl;
-	getline(cin, inputFileName);
+    string inputFileName;
+    string compressedFile, encodedFile;
+    string task;
     Huffman h;
-    h.makeTree(inputFileName); 
+    cout << "Would you like to encode or decode your file? " << endl;
+    getline(cin, task);
+    if (task == "encode" || task == "Encode" || task == "E" || task == "e"){
+	    cout << "Please enter the name of an input file: " << endl;
+    	    getline(cin, inputFileName);
+	    h.makeTree(inputFileName);
+    }else{
+    	if (task == "decode" || task == "Decode" || task == "D" || task == "d"){
+	    cout << "Please enter the name of a compressed input file (.hzip file): " << endl;
+	    getline(cin, compressedFile);
+	    cout << "Please enter the name of an encoded input file (.hcodes file): " << endl;
+	    getline(cin, encodedFile);
+            //produce a decompressed file
+	}else{
+		cout << "If you wish to encode your file, enter encode, or e." << endl;
+		cout << "If you wish to decode your file, enter decode, or d." << endl;
+		getline(cin, task);
+	}
+    }
     char * test = new char[8];
     test[0] = '0';
     test[1] = '0';
@@ -43,5 +61,11 @@ int main(){
     char * test3 = h.encode("abcabcabc");
     cout << test3 << endl;
     cout << h.decode(test3) << endl;
-	return 0;
+
+
+     //output encoded file should be named: filename.hzip
+     //output compressed file: filename.hcodes
+     // print the compression ratio (# of bits in compressed file /  total # in original file)
+
+    return 0;
 }

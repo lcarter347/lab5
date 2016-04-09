b1f55727dfa9bfa89aab7425940c552e63102f51
Hannah Zontine <hzontine@Hannahs-MBP.home>
2016-04-09 12:12:15 -0400

    Added file checking

--- main.cpp
+++ main.cpp
@@ -18,22 +18,30 @@ int main(){
     Huffman h;
     cout << "Would you like to encode or decode your file? " << endl;
     getline(cin, task);
+    while(true){
     if (task == "encode" || task == "Encode" || task == "E" || task == "e"){
 	    cout << "Please enter the name of an input file: " << endl;
-    	    getline(cin, inputFileName);
+   	    getline(cin, inputFileName);
 	    h.makeTree(inputFileName);
+	    break;
     }else{
     	if (task == "decode" || task == "Decode" || task == "D" || task == "d"){
 	    cout << "Please enter the name of a compressed input file (.hzip file): " << endl;
 	    getline(cin, compressedFile);
 	    cout << "Please enter the name of an encoded input file (.hcodes file): " << endl;
 	    getline(cin, encodedFile);
-            //produce a decompressed file
+	    if(compressedFile.find(".hzip") != -1  && encodedFile.find(".hcodes") != -1){
+
+	            //produce a decompressed file
+	    }    
+
+	    break;
 	}else{
 		cout << "If you wish to encode your file, enter encode, or e." << endl;
 		cout << "If you wish to decode your file, enter decode, or d." << endl;
 		getline(cin, task);
 	}
+    }
     }
     char * test = new char[8];
     test[0] = '0';
@@ -65,7 +73,7 @@ int main(){
 
      //output encoded file should be named: filename.hzip
      //output compressed file: filename.hcodes
-     // print the compression ratio (# of bits in compressed file /  total # in original file)
+     //print the compression ratio (# of bits in compressed file /  total # in original file)
 
     return 0;
 }

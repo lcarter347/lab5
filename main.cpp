b04219400e304f55b27b34320fd15525d0709ef0
Hannah Zontine <hzontine@mail.umw.edu>
2016-04-14 11:39:43 -0400

    Added compression ratio

--- main.cpp
+++ main.cpp
@@ -24,44 +24,43 @@ int main(){
     cout << "Would you like to encode or decode your file? " << endl;
     getline(cin, task);
     while(true){
-    if (task == "encode" || task == "Encode" || task == "E" || task == "e"){
-	    cout << "Please enter the name of an input file: " << endl;
-   	    getline(cin, inputFileName);
-	    string outputFileName = inputFileName + ".hzip";
-	    fileAsString = compute(inputFileName);
-	    calcweights(inputFileName);
-	    string weightsFile = inputFileName + ".w";
-	    h->makeTree(weightsFile);
-	    char* answer = h->encode(fileAsString);
-	    h->setMessageLength(strlen(answer));
-	    writeBin(answer, outputFileName);
-	    string str = inputFileName + ".hcodes";
-	    h->writeTreeToFile(str, h->getMessageLength());
-	    break;
-    }else{
-    	if (task == "decode" || task == "Decode" || task == "D" || task == "d"){
-	    cout << "Please enter the name of a compressed input file (.hzip file): " << endl;
-	    getline(cin, compressedFile);
-	    cout << "Please enter the name of an encoded input file (.hcodes file): " << endl;
-	    getline(cin, encodedFile);
-	    if(compressedFile.find(".hzip") != -1  && encodedFile.find(".hcodes") != -1){
-		    h->makeTreeFromFile(encodedFile);
-		    char* value = readBin(compressedFile, h->getMessageLength());
-	            string g = h->decode(value);
-		    cout << g << endl;
+	    if (task == "encode" || task == "Encode" || task == "E" || task == "e"){
+		    cout << "Please enter the name of an input file: " << endl;
+		    getline(cin, inputFileName);
+		    string outputFileName = inputFileName + ".hzip";
+		    fileAsString = compute(inputFileName);
+		    calcweights(inputFileName);
+		    string weightsFile = inputFileName + ".w";
+		    h->makeTree(weightsFile);
+		    char* answer = h->encode(fileAsString);
+		    h->setMessageLength(strlen(answer));
+		    writeBin(answer, outputFileName);
+		    string str = inputFileName + ".hcodes";
+		    h->writeTreeToFile(str, h->getMessageLength());
+		    int length = h->getMessageLength();
+		    double fileSize = fileAsString.length() * 8.0;
+		    cout << "The compression ratio is: " << length/fileSize << endl;
+		    break;
+	    }else{
+		if (task == "decode" || task == "Decode" || task == "D" || task == "d"){
+		    cout << "Please enter the name of a compressed input file (.hzip file): " << endl;
+		    getline(cin, compressedFile);
+		    cout << "Please enter the name of an encoded input file (.hcodes file): " << endl;
+		    getline(cin, encodedFile);
+		    if(compressedFile.find(".hzip") != -1  && encodedFile.find(".hcodes") != -1){
+			    h->makeTreeFromFile(encodedFile);
+			    char* value = readBin(compressedFile, h->getMessageLength());
+			    string g = h->decode(value);
+			    cout << g << endl;
+		    }
+		    break;
+		}else{
+			cout << "If you wish to encode your file, enter encode, or e." << endl;
+			cout << "If you wish to decode your file, enter decode, or d." << endl;
+			getline(cin, task);
+		}
 	    }
-	    break;
-	}else{
-		cout << "If you wish to encode your file, enter encode, or e." << endl;
-		cout << "If you wish to decode your file, enter decode, or d." << endl;
-		getline(cin, task);
-	}
     }
-    }
-    //output encoded file: filename.hzip
-     //output compressed file: filename.hcodes
-     //print the compression ratio (# of bits in compressed file /  total # in original file)
-
     return 0;
 }
 
